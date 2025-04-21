c6ebd0d93a8cd786489253549b735bdfd0630bfd
Yntymak <yntymak.almazbekuulu1@gmail.com>
2025-04-22 01:10:03 +0600

    h

---
+++ script.js
@@ -0,0 +1,30 @@
+// console.log('hello world')
+// console.log(1)
+// console.log(3, 'aibek')
+// console.log('aibek', 3)
+
+// this is a command
+/*hello*/
+
+
+//22.04.2025
+
+let name = 'yntymak'
+console.log(name)
+name = 'aku'
+console.log(name)
+
+
+const age = 12
+//age = 5 
+console.log(age)
+
+const name1 = 'yntymak', 
+age1 =20, 
+isGood = true
+
+//always have to use let or const
+//const can't be changed since it's constant variable
+//var is old one, almost doesn't used 
+
+/////////////////////////////////////////////////////////////////
