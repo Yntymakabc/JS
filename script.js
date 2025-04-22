b1c929aa662c53f75a62134ab734d9d30071e73b
Yntymak <yntymak.almazbekuulu1@gmail.com>
2025-04-22 13:24:01 +0600

    df

--- script.js
+++ script.js
@@ -10,14 +10,14 @@
 //22.04.2025
 
 let name = 'yntymak'
-console.log(name)
+//console.log(name)
 name = 'aku'
-console.log(name)
+//sconsole.log(name)
 
 
 const age = 12
 //age = 5 
-console.log(age)
+//console.log(age)
 
 const name1 = 'yntymak', 
 age1 =20, 
@@ -28,3 +28,63 @@ isGood = true
 //var is old one, almost doesn't used 
 
 /////////////////////////////////////////////////////////////////
+
+
+const phone = "samsung"
+let pen = 'black'
+const age4 = 3
+
+
+const a = 1
+let b = 2
+//console.log(`${a} and he is ${a+b}`)
+
+
+const country = "russion"
+//console.log(typeof country)//no parenheses
+
+const c = '3'
+//console.log(typeof Number(c))
+
+//console.log(4 == '4')
+//console.log(5 === '5') false
+
+
+const q = 2020
+const message = q===2020 ? 'this is the mans world' : 'thsi is womans world'
+//console.log(message)
+
+
+/*const year = 3000;
+
+const message = year === 2024
+  ? 'Начинаю учиться!'
+  : year === 3000
+    ? 'Нейросети победили'
+    : 'Неизвестный год :/';
+
+console.log(message);
+*/
+
+
+//console.log(true&false)
+
+//////////OROPERATION
+//const e = 15;
+//const withParend = false
+//if(e > 16 || withParend){
+//    console.log('you can enter the bar')
+//}
+//else(
+//    console.log('go and come next year')
+//)
+
+// || - or operation, && - and operation, ! - not(!true === false )
+
+const f = null
+const g = 8
+const result2 = f ?? g
+console.log(result2)
+
+//“If the first value is not null or undefined, return it.
+//Otherwise, return the second value.”
